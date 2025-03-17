91ca5b2c8b0b8485f67a1355f5d3177bd6622a6e
DESKTOP-PQGUBOJ\Nam Nguyen <nguyenduynamnd95@gmail.com>
2025-03-17 16:43:26 +0700

    add HomePage component

---
+++ src/component/HomePage.tsx
@@ -0,0 +1,15 @@
+import { DatePicker } from 'antd';
+
+
+
+const HomePage = () => {
+
+    return (
+        <>
+            <div>This is Home Page</div>
+            <DatePicker />
+        </>
+        
+    )
+}
+export default HomePage;
